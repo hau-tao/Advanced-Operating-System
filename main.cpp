ec92f940466999d09c8dd60b87fc2e3596467283
005393943 <005393943@jb358-19.cse.csusb.edu>
2016-05-05 11:31:14 -0700

    process

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <algorithm>
+#include "Process.h"
+#include "ProcessTable.h"
+using namespace std;
+int main(){
+   ProcessTable PT ;
+   Process p1 = Process(PT.get_pid(), "process_1", 30, "run" , 5, 2, 3000, 1);
+   
+   // add p1 to PT
+   PT.add_process(p1);
+   // create p2
+   Process p2 = Process(PT.get_pid(), "process_2", 30, "run" , 5, 2, 3000, 1);
+   // addd p2 to PT
+   PT.add_process(p2);
+   cout <<"The size of table: "<< PT.get_size()<< endl; // should be 2
+   
+
+ /*   Process p1= Process( 1005, "process_1", 30, "run" , 5, 2, 3000, 1); */
+   
+    cout <<"My pid_1: "<< p1.get_pid()<<endl;
+    cout << "My name: "<<p1.get_program_name()<< endl;
+    cout << "My ttl: "<<p1.get_ttl()<<endl;
+    cout << "My_state: "<<p1.get_state()<<endl;
+    cout << "My burst time: "<<p1.get_burst()<<endl;
+    cout << "My io burst time: "<<p1.get_ioburst()<<endl;
+    cout << "My memory address: "<< p1.get_memoryAddress()<<endl;
+    cout << "My memory size: "<<p1.get_memorySize()<< endl; 
+    // remove process p1 from table
+    PT.remove_process(p1);
+    // remove process p1 from table
+    PT.remove_process(p2);
+    
+    cout <<"The size of table: "<< PT.get_size()<< endl; // shoule be 0
+    
+
+    return 0;
+}
+
